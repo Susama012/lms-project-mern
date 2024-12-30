70f2e8ccc4af42747595fef2bc9a723d2efdc8f9
Susama012 <susamaswaraj1999@gmail.com>
2024-12-30 09:48:40 +0530

    Added react-hot-toast

--- README.md
+++ README.md
@@ -64,4 +64,13 @@
 npm install @reduxjs/toolkit react-redux react-router-dom react-icons
 react-chartjs-2 chart.js daisyui axios react-hot-toast
 @tailwindcss/line-clamp
+
+```
+
+### configure auto import sort eslint 
+
+1. install simple sort 
+```
+   npm i -D eslint-plugin-simple-import-sort
+
 ```
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
+
+import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
-    <App />
-  // </StrictMode>,
+ <BrowserRouter>
+  <App />
+  <Toaster />
+ </BrowserRouter>
+  
+ 
 )
